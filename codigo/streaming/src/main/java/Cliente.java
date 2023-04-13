298f595a208e74e9e420e3adcc13ae3e05184fdb
ClaudioJansen <caujansen@gmail.com>
2023-04-12 22:05:12 -0300

    Criado metodos e atributos de acordo com o diagrama

--- codigo/streaming/src/main/java/Cliente.java
+++ codigo/streaming/src/main/java/Cliente.java
@@ -1,3 +1,36 @@
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class Cliente {
+    private String nomeDeUsuario;
+    private String senha;
+    private List<Serie> listaParaVer;
+    private List<Serie> listaJaVistas;
+
+    public void adicionarNaLista(Serie serie){
+
+    }
+
+    public void retirarDaLista(String nomeSerie){
+
+    }
+
+    public List<Serie> filtrarPorGenero(String genero){
+        return null;
+    }
+
+    public List<Serie> filtrarPorIdioma(String idioma){
+        return null;
+    }
+
+    public List<Serie> filtrarPorQtdEpisodios(int quantEpisodios){
+        return null;
+    }
+
+    public void registrarAudiencia(Serie serie){
+
+    }
 
 }
--- codigo/streaming/src/main/java/Serie.java
+++ codigo/streaming/src/main/java/Serie.java
@@ -1,3 +1,17 @@
-public class Serie {
+import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
+public class Serie {
+    private final String[] GENEROS = {"Terror", "Romance", "Drama", "Comedia", "Suspense", "Animacao", "Ficcao Cientifica"};
+    private String nome;
+    private String genero;
+    private String idioma;
+    private int quantidadeEpisodios;
+    private int audiencia;
+
+    public void registrarAudiencia(){
+
+    }
 }
